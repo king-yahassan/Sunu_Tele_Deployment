b0a01909d37323a2d17cc1c72aacb64187d37479
king-yahassan <king.yahassane@gmail.com>
2023-11-20 23:19:57 +0000

    Deployment: cofiguration path's Apis in the frontend

--- server/server.js
+++ server/server.js
@@ -21,6 +21,9 @@ const port = process.env.PORT || 4000
 //APIs are available in http://localhost:5000/api/ or in http://localhost:4000/api/ in local
 // and in https://sunu-tele.onrender.com 
 app.use("/api",routes)
+app.use('/',(req,res)=>{
+    res.send(`<h1>Welcome to Sunu Tele API</h1><p>This is a RESTful API for sunu tele application.</p>`)
+})
 
 //listening 
 app.listen(port, ()=>{
